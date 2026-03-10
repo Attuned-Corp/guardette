f98f1b7c9cfed98f768fa75b4edde18ee344fa2b
Jerry Bao <jerry@span.app>
2026-03-10 11:42:33 -0700

    feat: require CLIENT_SECRET and return 401 for auth errors

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,16 @@
-import pytest
+import os
 
+import pytest
 from starlette.datastructures import MutableHeaders
+
 from guardette.actions import ActionContext
 from guardette.datastructures import ProxyRequest, ProxyResponse
 from guardette.config import ConfigManager
 from guardette.secrets import ConfigSecretsManager
+
+
+def pytest_configure(config):
+    os.environ.setdefault("CLIENT_SECRET", "test")
 
 
 @pytest.fixture
